6f8d582bf31c6eeebac57c00a55e36c9737a131e
David Behar <david@behar.io>
2020-04-01 22:53:37 -0600

    Exercise twilio

---
+++ README.md
@@ -0,0 +1,13 @@
+Renombra los archivos que tienen un template.json, quitando el .template,
+
+- privatekey.template.json --> privatekey.json (Llénalos desde lo que se exporta
+  en google cloud)
+- secrets.template.json --> secrets.json 
+
+además pon las claves reales
+
+para correrlo usa uno de los 2 siguientes comandos
+
+## `npm run start`
+
+## `node a-message-sender/index.js`
---
+++ a-message-sender/index.js
@@ -0,0 +1,79 @@
+// A Message Sender
+
+const { google } = require('googleapis')
+
+// configure a JWT auth client
+const privatekey = require('./privatekey.json')
+
+const authClient = new google.auth.JWT(
+  privatekey.client_email,
+  null,
+  privatekey.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets.readonly'])
+
+// authentication
+authClient.authorize()
+  .then(function (tokens) {
+    // console.log(tokens);
+    console.log('Authentication successfull.\n')
+  })
+  .catch(function (error) {
+    throw (error)
+  })
+
+// things we shouldn’t share with our code
+const secrets = require('./secrets.json')
+
+const sheets = google.sheets('v4')
+sheets.spreadsheets.values.get({
+  // ID of the spreadsheet to retrieve data from
+  // A1 notation of the values to retrieve
+  // authorized client
+  spreadsheetId: secrets.spreadsheet_id,
+  range: 'Sheet1!A2:D',
+  auth: authClient
+})
+  .then(function (response) {
+    const rows = response.data.values || []
+    if (rows.length) {
+      sendMessage(rows)
+    }
+  })
+  .catch(function (err) {
+    console.log(err)
+  })
+
+const accountSid = secrets.account_sid
+const authToken = secrets.auth_token
+const client = require('twilio')(accountSid, authToken)
+const sandboxNumber = secrets.sandbox_number
+
+function sendMessage (rows) {
+  // stop condition
+  if (!rows.length) {
+    console.log('---------------------------------')
+    return
+  }
+
+  // take information from the first row
+  const firstRow = rows.shift()
+  const appointmentDate = firstRow[0]
+  const appointmentTime = firstRow[1]
+  const clientName = firstRow[2]
+  const phoneNumber = firstRow[3]
+
+  // send the message
+  client.messages
+    .create({
+      from: 'whatsapp:' + sandboxNumber,
+      to: 'whatsapp:' + phoneNumber,
+      body: `Hola ${clientName}, David Behar te ha mandado un mensaje de manera automática.`
+    })
+    .then(function (message) {
+      console.log(message.sid + '\n')
+      sendMessage(rows)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+}
